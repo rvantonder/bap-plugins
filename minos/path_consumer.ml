682b0c110a5eb83e3344cf98b85e74fffa5bd9dc
Rijnard <rvantonder@gmail.com>
2015-11-13 22:40:09 +0000

    why do i not see taints

--- minos/path_consumer.ml
+++ minos/path_consumer.ml
@@ -1,6 +1,7 @@
 open Core_kernel.Std
 open Bap.Std
 open Check
+open Cabs
 
 module SM = Monad.State
 open SM.Monad_infix
@@ -275,6 +276,163 @@ class type result = object
   method taints_of_term : tid -> Taint.taints Var.Map.t
 end
 
+(** ABI stuff *)
+
+type arg_size =
+  | Word                        (** same size as CPU word  *)
+  | Size of Size.t              (** the specified size     *)
+
+type pos =
+  | Ret_0
+  | Ret_1
+  | Arg of int
+
+type arg = {
+  arg_name : string;
+  arg_pos  : pos;
+  arg_intent : intent option;
+  arg_size : arg_size;
+}
+
+let ret_word n = {
+  arg_pos = n;
+  arg_name = "result";
+  arg_intent = Some Out;
+  arg_size = Word; (* compiler will cast return value itself *)
+}
+
+include struct
+  let stack mem sp endian sz off =
+    let width = Size.to_bits sz in
+    let off = Word.of_int ~width (off * (Size.to_bytes sz)) in
+    let mem = Bil.var mem in
+    let addr = if Word.is_zero off
+      then Bil.(var sp)
+      else Bil.(var sp + int off) in
+    Bil.load ~mem ~addr endian sz
+
+  let arm_stack = ARM.CPU.(stack mem sp LittleEndian `r32)
+  let x86_stack = IA32.CPU.(stack mem sp LittleEndian `r32)
+  let x64_stack = AMD64.CPU.(stack mem sp LittleEndian `r64)
+
+  open Bil
+  let abi = function
+    | #Arch.arm ->
+      ARM.CPU.(function
+          | Ret_0 -> var r0
+          | Ret_1 -> var r1
+          | Arg 0 -> var r0
+          | Arg 1 -> var r1
+          | Arg 2 -> var r2
+          | Arg 3 -> var r3
+          | Arg n -> arm_stack Int.(n-4))
+    | `x86_64 ->
+      AMD64.CPU.(function
+          | Ret_0 -> var rax
+          | Ret_1 -> var rdx
+          | Arg 0 -> var rdi
+          | Arg 1 -> var rsi
+          | Arg 2 -> var rdx
+          | Arg 3 -> var rcx
+          | Arg 4 -> var r.(0)
+          | Arg 5 -> var r.(1)
+          | Arg n -> x64_stack Int.(n-6))
+    | `x86 ->
+      IA32.CPU.(function
+          | Ret_0 -> var rax
+          | Ret_1 -> var rdx
+          | Arg n -> x86_stack Int.(n+1))
+    | _ -> raise Not_found
+end
+
+let (>:) s1 s2 e =
+  match Size.(to_bits s2 - to_bits s1) with
+  | 0 -> e
+  | n when n > 0 -> Bil.(cast high n e)
+  | n -> Bil.(cast low n e)
+
+let term_of_arg arch sub {arg_name; arg_size; arg_intent; arg_pos} =
+  let word_size = Arch.addr_size arch in
+  let size = match arg_size with
+    | Word -> (word_size :> Size.t)
+    | Size size -> size in
+  let typ = Type.imm (Size.to_bits size) in
+  let exp = try abi arch arg_pos with
+      exn -> Bil.unknown "unkown abi" typ in
+  (* so far we assume, that [abi] returns expressions of word size,
+     if this will ever change, then we need to extend abi function
+     to return the size for us. *)
+  let exp = (word_size >: size) exp in
+  let var = Var.create (Sub.name sub ^ "_" ^ arg_name) typ in
+  Arg.create ?intent:arg_intent var exp
+
+let fill_args arch fns program =
+  Term.map sub_t program ~f:(fun sub ->
+      match Map.find fns (Sub.name sub) with
+      | None -> sub
+      | Some args ->
+        List.fold args ~init:sub ~f:(fun sub arg ->
+            Term.append arg_t sub (term_of_arg arch sub arg)))
+
+let intent_of_type = function
+  | PTR (CONST _) -> Some In
+  | PTR (_) -> Some Both
+  | _ -> Some In
+
+let size_of_type typ = match typ with
+  | PTR _ -> Word
+  | CHAR _ -> Size `r8
+  | INT (SHORT,_) -> Size `r16
+  | INT (LONG_LONG,_) -> Size `r64
+  | FLOAT _ -> Size `r32
+  | DOUBLE false -> Size `r64
+  | DOUBLE true -> Size `r128
+  | _ -> Word
+
+let arg_of_name n = function
+  | (_,VOID,_,_) -> None
+  | (name,typ,_,_) -> Some {
+      arg_pos = Arg n;
+      arg_name = if name <> "" then name else sprintf "x%d" (n+1);
+      arg_intent = intent_of_type typ;
+      arg_size = size_of_type typ;
+    }
+
+let string_of_single_name n (_,_,name) = arg_of_name n name
+
+let args_of_single_names = List.filter_mapi ~f:string_of_single_name
+
+let push = List.map ~f:(fun a -> match a with
+    | {arg_pos = Arg n} -> {a with arg_pos = Arg (n+1)}
+    | a -> a)
+
+let fn_of_definition = function
+  | DECDEF (_,_,[(name, PROTO (ret,args,false),[],NOTHING)])
+  | FUNDEF ((_,_,(name, PROTO (ret,args,false),[], NOTHING)), _) ->
+    let args = args_of_single_names args in
+    let args = match ret with
+      | VOID -> args
+      | STRUCT _ | CONST (STRUCT _) ->
+        {(ret_word (Arg 0)) with arg_intent = Some In} :: push args
+      | INT (LONG_LONG,_) -> ret_word Ret_0 :: ret_word Ret_1 :: args
+      | _ -> ret_word Ret_0 :: args in
+    Some (name,args)
+  | _ -> None
+
+let fns_of_definitions =
+  List.fold ~init:String.Map.empty ~f:(fun fns defn ->
+      match fn_of_definition defn with
+      | None -> fns
+      | Some (name,data) -> Map.add fns ~key:name ~data)
+
+let args_of_file file =
+  let open Frontc in
+  match Frontc.parse_file file stderr with
+  | PARSING_ERROR -> raise Parsing.Parse_error
+  | PARSING_OK ds -> fns_of_definitions ds
+
+(** /ABI stuff *)
+
 let display_tainted ctxt tid =
   let taint_vars = ctxt#taints_of_term tid in
   Format.printf "Displaying taint for sink tid %a\n%!" Tid.pp tid;
@@ -296,14 +454,28 @@ let seed t =
 let consume sub_path (check : Check.t) (ctxt : Check.ctxt) =
   (** Wrap sub_path in a program term so it doesn't complain about
       entry point not found. It still wants a sub within program though *)
-  let p =
+  let prog =
     let new_p = Program.Builder.create () in
     Program.Builder.add_sub new_p sub_path;
+    (** Also add sprintf/system calls *)
+    (**let sprintf_tid = Tid.(!"@sprintf") in
+    let system_tid = Tid.(!"@system") in
+    let sub1 = Util.sub_of_tid ctxt.project sprintf_tid |> Util.val_exn in
+    let sub2 = Util.sub_of_tid ctxt.project system_tid |> Util.val_exn in
+    Program.Builder.add_sub new_p sub1;
+    Program.Builder.add_sub new_p sub2;*)
     Program.Builder.result new_p in
-  Format.printf "Path (as program): \n \
-                 %a\n====================\n" Program.pp p;
 
-  let p = Term.map sub_t p ~f:(fun sub ->
+  (**
+  let arch = Project.arch ctxt.project in
+  let args = args_of_file "libc.h" in
+  let prog = fill_args arch args prog in
+  *)
+
+  Format.printf "Path (as program, with args): \n \
+                 %a\n====================\n" Program.pp prog;
+
+  let p = Term.map sub_t prog ~f:(fun sub ->
       Term.map blk_t sub ~f:(fun blk ->
           Term.map def_t blk ~f:(fun def ->
               Format.printf "Seeding %a:%a" Tid.pp (Term.tid def) Def.pp def;
@@ -317,21 +489,21 @@ let consume sub_path (check : Check.t) (ctxt : Check.ctxt) =
   let k = 500 in (** needed? *)
   (*let p = Project.program ctxt.project in*) (* Nope, you will use my program term *)
   (** This is the unified context over taint and biri *)
-  let taint_ctxt = new context p k in
+  let taint_ctxt = new context prog k in
   (** This is a mapping of sub to blk addrs *)
-  let mapping = create_mapping p in
+  let mapping = create_mapping prog in
   (** Fetch the value at a given address *)
   let memory = memory_lookup ctxt.project in
   (** The interpreter, instantiated with context *)
   let biri = new main def_summary memory mapping def_const in
   let map _ = None in (** no idea *)
-  let res = run_from_point map p biri (`Term start_sub) in
+  let res = run_from_point map prog biri (`Term start_sub) in
   let exec_res = (SM.exec res taint_ctxt :> result) in
   (** Check taint of first def in the last (sink) blk *)
-  let first_sub = Term.first sub_t p |> Util.val_exn in
+  let first_sub = Term.first sub_t prog |> Util.val_exn in
   (*let tid = Util.blk_of_tid first_sub ctxt.trim.sink_tid |> Term.first def_t |>
             Util.val_exn |> Term.tid in*)
-  let all_tids = Term.enum sub_t p |> Seq.fold ~init:Seq.empty ~f:(fun acc sub ->
+  let all_tids = Term.enum sub_t prog |> Seq.fold ~init:Seq.empty ~f:(fun acc sub ->
       Term.enum blk_t sub |> Seq.fold ~init:acc ~f:(fun acc blk ->
           Term.enum def_t blk |> Seq.fold ~init:acc ~f:(fun acc def ->
               (Term.tid def) ^:: acc))) in
